e8a0e0593b63b02913c09e14dbfd9b335db0417b
LitaoYang-Jet <69187527+LitaoYang-Jet@users.noreply.github.com>
2021-05-10 16:45:42 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from octid import octid
-classify_model = octid.octid(model = 'googlenet', customised_model = False, feature_dimension = 3, outlier_fraction_of_SVM = 0.03, traning_dataset='small_samples/training_dataset', validation_dataset='small_samples/validation_dataset', unlabeled_dataset=None)
+classify_model = octid.octid(model = 'googlenet', customised_model = False, feature_dimension = 3, outlier_fraction_of_SVM = 0.03, training_dataset='small_samples/training_dataset', validation_dataset='small_samples/validation_dataset', unlabeled_dataset=None)
 classify_model()
